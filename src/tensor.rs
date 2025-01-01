02eb5002428c1fab0f3f40539f771b72af0123fc
Motsepe-Jr <hectormotsepe@gmail.com>
2025-01-01 20:25:42 +0200

    intial commit

---
+++ src/tensor.rs
@@ -0,0 +1,22 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use ndarray::{Array, ArrayD};
+use uuid::Uuid;
+
+
+#[derive(Debug)]
+pub struct Function {
+    pub name: String, 
+    pub backward: Box<dyn Fn(&[Rc<RefCell<Tensor>>], &ArrayD<f32>) -> Vec<Option<ArrayD<f32>>>>,
+}
+
+#[derive(Debug)]
+pub struct Tensor {
+    pub id: Uuid,
+    pub data: ArrayD<f32>,
+    pub grad: Option<ArrayD<f32>>,
+    pub requires_grad: bool, 
+    pub is_leaf: bool, 
+    pub grad_fn: Option<Rc<Function>>,
+    
+}
